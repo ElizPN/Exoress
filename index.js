fa2056fed6457fa612884aab311f2e80ad41f7d7
ElizPN <pankevych.liza@gmail.com>
2023-04-12 11:34:14 +0200

    404 handling

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.get("/1/", function (req, res) {
   res.send("page 1");
   console.log(req.path);
   console.log(req.url);
+  console.log(req.originalUrl);
+  console.log(req.query);
+  console.log(req.query.get1);
+  console.log(req.protocol);
+  console.log(req.secure);
+  console.log(req.headers);
+  console.log(req.acceptsLanguages);
+  console.log(req.ip);
 });
 
 app.get("/2/", function (req, res) {
@@ -19,6 +27,12 @@ app.get("/2/", function (req, res) {
 app.get("/3/", function (req, res) {
   res.send("page 3");
 });
+
+//handle 404
+app.use((req,res)=> {
+    res.status(404).send("Not found")
+
+})
 
 app.listen(3000, function () {
   console.log("runing");
