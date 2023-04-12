444e999e7ac20e7e471db741e70384130e37b3e8
ElizPN <pankevych.liza@gmail.com>
2023-04-12 16:38:25 +0200

    routing to different pages

--- page.js
+++ page.js
@@ -6,6 +6,12 @@ const app = express();
 app.get("/page/", (req, res) => {
   res.sendFile(__dirname + "/page.html");
 });
+app.get("/page1/", (req, res) => {
+  res.sendFile(__dirname + "/page1.html");
+});
+app.get("/page2/", (req, res) => {
+  res.sendFile(__dirname + "/page2.html");
+});
 
 app.use((req, res) => {
   res.status(404).send("omm page not found");
