a526241f92dda6d388402be553b3fec5c1e9c39f
ElizPN <pankevych.liza@gmail.com>
2023-04-12 16:45:23 +0200

    change export and import

--- __dirname.js
+++ __dirname.js
@@ -1,6 +1,4 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-import * as url from "url";
-
-const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
-
-export default __dirname;
+export const __dirname = dirname(fileURLToPath(import.meta.url));
--- page.js
+++ page.js
@@ -1,5 +1,5 @@
 import express from "express";
-import __dirname from "./__dirname.js";
+import { __dirname } from "./__dirname.js";
 
 const app = express();
 
