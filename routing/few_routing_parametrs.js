c07c900ffacecd03ff895f29f4878da935b80057
ElizPN <pankevych.liza@gmail.com>
2023-04-13 14:53:46 +0200

    few routing parametrs

---
+++ routing/few_routing_parametrs.js
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+
+app.get("/test/:num1/:num2", (req, res) => {
+  //   res.send(req.params.num1 + " " + req.params.num2);
+  const number1 = parseInt(req.params.num1);
+  const number2 = parseInt(req.params.num2);
+  let sum = number1 + number2;
+  sum = sum.toString();
+
+  res.send(sum);
+});
+
+app.listen(3000);
