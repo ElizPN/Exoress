9505d6718c94faf48da07b7162b66e2c16fc9c7f
ElizPN <pankevych.liza@gmail.com>
2023-04-14 14:37:37 +0200

    handle 404 in use

--- routing/hadle_404.js
+++ routing/hadle_404.js
@@ -10,4 +10,8 @@ app.get("/test/:num", (req, res) => {
     res.status(404).send("page not found");
   }
 });
+
+app.use((req, res)=> {
+    res.status(404).send("Page not found")
+})
 app.listen(3000);
