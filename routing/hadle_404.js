d6edcb5cd799d082c47039ed3223bcb3064fd5f6
ElizPN <pankevych.liza@gmail.com>
2023-04-14 11:08:57 +0200

    check number of routing and handle 404

---
+++ routing/hadle_404.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+
+app.get("/test/:num", (req, res) => {
+  let num = req.params.num;
+
+  if (num >= 1 && num <= 9) {
+    res.send("your num" + num);
+  } else {
+    res.status(404).send("page not found");
+  }
+});
+app.listen(3000);
