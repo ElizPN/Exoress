63ff40466fc412877b2bbdcdfc53e74d1b597863
ElizPN <pankevych.liza@gmail.com>
2023-04-16 09:10:06 +0200

    404 handling

--- routing/routing_to_other_files.js
+++ routing/routing_to_other_files.js
@@ -1,11 +1,19 @@
 import express from "express";
 import { __dirname } from "./__dirname.js";
+import fs from "fs/promises";
+import { constants } from "fs";
 
 const app = express();
 
-app.get("/page/:num", (req, res) => {
-  res.sendFile(__dirname + "/pages/" + req.params.num + ".html");
-  console.log(req.params.num);
+app.get("/page/:num", async (req, res) => {
+  const path = __dirname + "/pages/" + req.params.num + ".html";
+
+  try {
+    await fs.access(path, constants.F_OK);
+    res.sendFile(path);
+  } catch (error) {
+    res.status(404).send("Page not found");
+  }
 });
 
 app.listen(3000);
