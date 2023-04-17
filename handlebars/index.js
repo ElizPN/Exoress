bfba011fab69c28f247f477438547add282f07a1
ElizPN <pankevych.liza@gmail.com>
2023-04-17 11:26:10 +0200

    handle exceptions (404)

--- handlebars/index.js
+++ handlebars/index.js
@@ -1,18 +1,32 @@
 import expressHandlebars from "express-handlebars";
+import express from "express";
+import { __dirname } from "./__dirname.js";
+import fs from "fs/promises";
+import { constants } from "fs";
 
 const handlebars = expressHandlebars.create({
   defaultLayout: "main",
   extname: "hbs",
 });
 
-import express from "express";
 const app = express();
 
 app.engine("hbs", handlebars.engine);
 app.set("view engine", "hbs");
 
-app.get("/page/:page/", (req, res) => {
-  res.render(req.params.page);
+app.get("/page/:page/", async (req, res) => {
+  const path = __dirname + "/views/" + req.params.page + ".hbs";
+
+  try {
+    await fs.access(path, constants.F_OK);
+    res.render(req.params.page);
+  } catch (error) {
+    res.status(404).render("error");
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).render("error");
 });
 
 app.listen(3000);
